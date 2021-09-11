c480e873c04424099ab88cc291da13bf04c37755
Patrick Craig <patrick.craig@me.com>
2021-09-11 11:15:09 -0400

    adding local ids and tokens

--- utils/contentfulPosts.js
+++ utils/contentfulPosts.js
@@ -1,13 +1,16 @@
 const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
 const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
 
-const client = require('contentful').createClient({
+const contentful = require('contentful');
+
+const client = contentful.createClient({
   space: space,
   accessToken: accessToken,
 })
 
 export async function fetchEntries() {
   const entries = await client.getEntries()
+  console.log('entries: ', entries)
   if (entries.items) return entries.items
   console.log(`Error getting Entries for ${contentType.name}.`)
 }
